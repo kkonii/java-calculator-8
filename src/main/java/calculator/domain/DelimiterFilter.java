9e25ce22aac8aefadf951ecf3802ae1cd9c4507f
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-16 23:17:14 +0900

    feat: 입력된 문자열에서 커스텀 구분자를 식별하는 기능 추가
    - 한 글자가 아니면 예외를 던지는 검증도 함께 추가

---
+++ src/main/java/calculator/domain/DelimiterFilter.java
@@ -0,0 +1,27 @@
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DelimiterFilter {
+
+    private static final String CUSTOM_FORMAT_MARKS = "/|\\\\n";
+    private static final int VALID_CUSTOM_LENGTH = 1;
+
+    public String filterCustomDelimiter(String input) {
+        List<String> filtered = Arrays.stream(input.split(CUSTOM_FORMAT_MARKS))
+                //잘리고 남은 공백은 버린다
+                .filter(f -> !f.isBlank())
+                .toList();
+
+        validateLength(filtered.getFirst());
+
+        return filtered.getFirst();
+    }
+
+    private void validateLength (String extracted) {
+        if (extracted.length() > VALID_CUSTOM_LENGTH) {
+            throw new IllegalArgumentException("커스텀 구분자는 " + VALID_CUSTOM_LENGTH + "개만 입력할 수 있습니다.");
+        }
+    }
+}
