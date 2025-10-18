2ccefaf2410b3e5747c26fc1c2ef6feb1592f415
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-18 12:57:22 +0900

    feat: 입력된 숫자들이 모두 양수인지 검증 추가

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -7,7 +7,14 @@ public class Numbers {
     private final List<Integer> numbers;
 
     public Numbers(List<Integer> numbers) {
+        numbers.forEach(this::validate);
         this.numbers = numbers;
+    }
+
+    private void validate(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("숫자는 양수만 입력 가능합니다.");
+        }
     }
 
     public int summarize() {
