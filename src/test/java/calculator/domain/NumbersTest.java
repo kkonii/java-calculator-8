fafb92503da4ef4f2e3ffb8a92f8534372c4d960
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-18 14:40:10 +0900

    test: Numbers 클래스에 대한 테스트 케이스 추가

---
+++ src/test/java/calculator/domain/NumbersTest.java
@@ -0,0 +1,23 @@
+package calculator.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumbersTest {
+
+    @Test
+    @DisplayName("[성공] 숫자들의 합을 구하는 데에 성공한다")
+    void sum_numbers() {
+        Numbers nums = new Numbers(List.of(1, 2, 3, 4));
+        Assertions.assertThat(nums.summarize()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("[예외] 양수가 아닌 숫자에 대해 에외를 발생시킨다")
+    void throw_when_not_positive() {
+        Assertions.assertThatThrownBy(() -> new Numbers(List.of(1, -2)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
