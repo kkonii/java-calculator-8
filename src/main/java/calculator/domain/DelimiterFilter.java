926c2f5a75eafcf1a1cb6b935b6129763d3ac573
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-18 16:41:00 +0900

    fix: 커스텀 입력 형식을 지키지 않고 구분자를 입력했을 때 검증이 되지 않는 문제 해결

--- src/main/java/calculator/domain/DelimiterFilter.java
+++ src/main/java/calculator/domain/DelimiterFilter.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class DelimiterFilter {
 
     private static final String CUSTOM_FORMAT_MARKS = "//|\\\\n";
+    private static final String DEFAULT_FORMAT_MARKS = "^[가-힣A-Za-z0-9,:]+$";
     private static final int VALID_CUSTOM_LENGTH = 1;
     private static final int DEFAULT_FORMAT_STATUS = 1;
     private static final int CUSTOM_FORMAT_STATUS = 2;
@@ -19,8 +20,9 @@ public class DelimiterFilter {
 
         if (splitInput.size() == DEFAULT_FORMAT_STATUS) {
             //기본 구분자 형식으로 작성된 문자열
-            List<String> defaultDelimiters = List.of(",", ":");
             String numbersInput = splitInput.getFirst();
+            validateDefaultFormat(numbersInput);
+            List<String> defaultDelimiters = List.of(",", ":");
 
             return new FilteredInputDto(defaultDelimiters, numbersInput);
         } else if (splitInput.size() == CUSTOM_FORMAT_STATUS) {
@@ -34,6 +36,12 @@ public class DelimiterFilter {
         } else {
             //잘못된 입력 형식
             throw new IllegalArgumentException("올바른 입력 형식이 아닙니다.");
+        }
+    }
+
+    private void validateDefaultFormat(String numberInput) {
+        if (!numberInput.matches(DEFAULT_FORMAT_MARKS)) {
+            throw new IllegalArgumentException("기본 구분자 외의 문자는 커스텀 형식으로 입력해 주세요.");
         }
     }
 
